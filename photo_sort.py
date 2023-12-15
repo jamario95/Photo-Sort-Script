d41f010ec6351cf41f524a3086878291756c1753
Mariusz Januszek <mariusz.januszek95@gmail.com>
2023-12-15 11:40:51 +0100

    format code

--- photo_sort.py
+++ photo_sort.py
@@ -1,36 +1,37 @@
 import os
 import shutil
 from datetime import datetime
-import sys
 import tkinter as tk
 from tkinter import filedialog
 
+
 def sort_by_date(folder_path):
-    #Listing all the files in the directory
+    # Listing all the files in the directory
     files = os.listdir(folder_path)
 
     # FIltering only for image files
     image_extensions = [".jpg", ".jepg", ".png"]
-    image_files = [file for file in files if os.path.splitext(file)[1].lower() in image_extensions]
+    image_files = [
+        file for file in files if os.path.splitext(file)[1].lower() in image_extensions
+    ]
 
-
-    #Sorting images by modification time
+    # Sorting images by modification time
     image_files.sort(key=lambda x: os.path.getmtime(os.path.join(folder_path, x)))
 
-    #Create a folder for each year
+    # Create a folder for each year
     for file in image_files:
         file_path = os.path.join(folder_path, file)
         modification_time = os.path.getmtime(file_path)
         date = datetime.fromtimestamp(modification_time)
         year = str(date.year)
 
-
-       # Create directory for each year if not exists
+        # Create directory for each year if not exists
         destination_folder = os.path.join(folder_path, year)
         os.makedirs(destination_folder, exist_ok=True)
 
         # Move the file to the respective folder
         shutil.move(file_path, os.path.join(destination_folder, file))
+
 
 def select_folder():
     root = tk.Tk()
@@ -41,14 +42,8 @@ def select_folder():
         print("Folder was not selected.")
 
     sort_by_date(folder_path)
-    print("Files sorted!") 
-
-
-
+    print("Files sorted!")
 
 
 if __name__ == "__main__":
-
-  select_folder()
-
-
+    select_folder()
